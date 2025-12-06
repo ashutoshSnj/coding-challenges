ffce98f21fb3be830ecd6bd32b6c8ae8ed55a195
Ashutosh Shelke <ashutoshshelke.snj@gmail.com>
2025-12-06 12:29:02 +0530

    Duplicate Logic Change Use List

--- com/array/remove_duplicate_elements.java
+++ com/array/remove_duplicate_elements.java
@@ -1,9 +1,14 @@
 package com.array;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class remove_duplicate_elements {
   public static void main(String[] args) {
-	int[] arr= {1,2,3,4,5,2,3,54};
-	int len=arr.length-1;
+	int[] arr= {1,2,3,4,5,2,2,3,54};
+/*	int len=arr.length-1;
 	for(int i=0;i<=len;i++) {
 		for (int j=i+1;j<=len;j++) {
 			if(arr[i]==arr[j]) {
@@ -19,5 +24,20 @@ public class remove_duplicate_elements {
 	for (int i=0;i<=len;i++) {
 		System.out.println(arr[i]);
 	}
+	*/
+	
+	List<Integer>list=new ArrayList<Integer>();
+    Arrays.stream(arr).forEach(ref->list.add(ref));
+	
+    for(int i=0;i<list.size();i++) {
+    	for(int j=i+1;j<list.size();j++) {
+    		if(list.get(i).equals(list.get(j))) {
+    			list.remove(j);
+    			j--;
+    			
+    		}
+    	}
+    }
+	list.stream().forEach(ref->System.out.println(ref));
 }
 }
